148a877b7af022cdcfc958811df0b60356c79bda
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2024-05-22 13:40:46 +0900

    user handler

---
+++ src/handlers/user.rs
@@ -0,0 +1,10 @@
+use axum::{Json, extract::Path};
+use crate::models::user::User;
+
+pub async fn list() -> Json<Vec<User>> {
+    Json(vec![])
+}
+
+pub async fn get(Path(id): Path<i64>) -> Json<User> {
+    Json(User { id, ..Default::default() })
+}
