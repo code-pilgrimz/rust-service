1f537142e5bc4c4fa5eb0b49701c02012a710f37
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2024-06-09 13:41:18 +0900

    project model

---
+++ src/models/project.rs
@@ -0,0 +1,10 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Clone, Default)]
+pub struct Project {
+    pub id: i64,
+    pub name: String,
+    pub description: String,
+    pub status: String,
+    pub archived: bool,
+}
