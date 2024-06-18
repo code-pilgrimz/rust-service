ac1a4b65cd7258288de910e7bc44e7b8579a6c50
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2024-06-18 18:12:24 +0900

    project handler

---
+++ src/handlers/project.rs
@@ -0,0 +1,10 @@
+use axum::{Json, extract::Path};
+use crate::models::project::Project;
+
+pub async fn list() -> Json<Vec<Project>> {
+    Json(vec![])
+}
+
+pub async fn get(Path(id): Path<i64>) -> Json<Project> {
+    Json(Project { id, ..Default::default() })
+}
