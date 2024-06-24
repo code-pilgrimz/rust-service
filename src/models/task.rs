290bb7f0b153fd5b0ba61c5b5ae4dddd80d9cf88
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2024-06-24 20:30:34 +0900

    task model

---
+++ src/models/task.rs
@@ -0,0 +1,10 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Clone, Default)]
+pub struct Task {
+    pub id: i64,
+    pub title: String,
+    pub priority: i64,
+    pub done: bool,
+    pub due_date: String,
+}
